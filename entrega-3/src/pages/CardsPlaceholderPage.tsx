30fc8d4ee43913ee0471c2dd7459d159d6eb36f7
Rafael Accetta <rafaelaccetta@id.uff.br>
2025-06-18 15:09:33 -0300

    add cardspage

---
+++ entrega-3/src/pages/CardsPlaceholderPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardsPlaceholderPage = () => {
+  return (
+    <div>CardsPlaceholderPage</div>
+  )
+}
+
+export default CardsPlaceholderPage
--- entrega-3/src/pages/CardsPorSlugCategoriaPage.tsx
+++ entrega-3/src/pages/CardsPorSlugCategoriaPage.tsx
@@ -1,4 +1,94 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import useRecuperarLivrosPorSlugCategoria from "../hooks/useRecuperarLivrosPorSlugCategoria";
+import Card from "../components/Card";
+import type Livro from "../interfaces/Livro"
+import CardsPlaceholderPage from "./CardsPlaceholderPage";
+
+
 export interface LivroCarrinho {
     idLivro: number;
     quantidade: number;
 }
+
+const CardsPorSlugCategoriaPage = () => {
+
+    const [carrinho, setCarrinho] = useState(() => {
+        const itensDeCarrinho = localStorage.getItem("carrinho");
+        return itensDeCarrinho ? JSON.parse(itensDeCarrinho) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("carrinho", JSON.stringify(carrinho))
+    }, [carrinho]);
+
+    const adicionarLivro = (livro: Livro) => {
+        setCarrinho((prevCarrinho: LivroCarrinho[]) => {
+            const existe = prevCarrinho.find((item) => item.idLivro === livro.id);
+            if(existe){
+                const novoCarrinho: LivroCarrinho[] = prevCarrinho
+                    .map((item: LivroCarrinho) => item.idLivro === livro.id ?
+                        {idLivro: item.idLivro, quantidade: item.quantidade + 1} : item);
+                return novoCarrinho;
+            } else {
+                return [...prevCarrinho, {idLivro: livro.id, quantidade: 1}]
+            }
+        });
+    };
+
+    const subtrairLivro = (livro: Livro) => {
+        setCarrinho((prevCarrinho: LivroCarrinho[]) => {
+            const existe = prevCarrinho.find((item) => item.idLivro === livro.id);
+            if (existe) {
+                const novoCarrinho: LivroCarrinho[] = prevCarrinho
+                    .map((item: LivroCarrinho) => item.idLivro === livro.id ? 
+                        {idLivro: item.idLivro, quantidade: item.quantidade - 1} : item);
+                return novoCarrinho.filter((item) => item.quantidade > 0);    
+            } else {
+                throw new Error("Erro ao subtrair 1 de livro no carrinho.");
+            }
+        });
+    };
+
+    const { slugCategoria } = useParams();
+    const {
+        data: livros,
+        isPending: carregandoLivros,
+        error: errorLivros
+    } = useRecuperarLivrosPorSlugCategoria(slugCategoria);
+
+    if(carregandoLivros) return <CardsPlaceholderPage/>;
+    if(errorLivros) throw errorLivros;
+
+    const livrosNoCarrinho: (LivroCarrinho | null)[] = [];
+    livros.forEach((livro) => {
+        const livroCarrinho = carrinho.find(
+            (item: LivroCarrinho) => item.idLivro === livro.id
+        );
+        livrosNoCarrinho.push(livroCarrinho ? livroCarrinho : null);
+    })
+    
+    return (
+    <>
+      <h5>
+        {slugCategoria
+          ? slugCategoria.charAt(0).toUpperCase() + slugCategoria.slice(1)
+          : "Produtos"}
+      </h5>
+      <div className="row">
+        {livros.map((livro, index) => (
+          <div key={livro.id} className="col-lg-2 col-md-3 col-sm-4 col-6">
+            <Card
+              livro={livro}
+              livroNoCarrinho={livrosNoCarrinho[index]}
+              adicionarLivro={adicionarLivro}
+              subtrairLivro={subtrairLivro}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+    );
+}
+
+export default CardsPorSlugCategoriaPage
